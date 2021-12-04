d4a1d414097cdfdd80a9934f18fec63f198a4960
Colin Clifton <cmc0169@auburn.edu>
2021-12-03 23:13:58 -0600

    Completed Shiny App
    Ran into too many roadblocks with the version that let the user pick which independent variables they wanted to use.
    
    This version has the user type in a "vector" for x and y. Once user presses the button the outputs for this package are printed, each separated in its own tab.

--- OptimizedRegression/app.R
+++ OptimizedRegression/app.R
@@ -1,97 +1,98 @@
 library(shiny)
 
-# devtools::install_github('AU-R-Programming/Final-Project-Group3/Group3Package/FinalProjPackage')
+devtools::install_github('AU-R-Programming/Final-Project-Group3/Group3Package/FinalProjPackage')
 library(FinalProjPackage)
 
 ui <- fluidPage(
 
-    titlePanel("Interactive Optimized Linear Regression Using 'mtcars' Set"),
+    titlePanel("Linear Regression Report Generator"),
 
     sidebarLayout(
         sidebarPanel(
-            checkboxGroupInput("independentVariables",
-                               "Select Independent Variables:",
-            c("Cylinders" = "cyl", "Horsepower" = "hp",
-              "Rear Axle Ratio" = "drat", "Weight" = "wt", "1/4 Mile Time" = "qsec",
-              "# of Forward Gears" = "gear", "# of Carburetors" = "carb")),
-            
-            actionButton("generate", "Generate Output")
+            ## Taking in an n-length vector from the user for x and y
+            textInput("xVector", "Enter X Vector (comma delimited)", 
+                      "1,2,5,2,8,2,3,4"),
+            textInput("yVector", "Enter Y Vector of equal length
+                       (comma delimited)", "0,2,4,6,10,1,3,2"),
+            actionButton("start", "Press to Generate")
         ),
-
+        
         mainPanel(
-           plotOutput("packagePlots"),
-           br(),
-           textOutput("beta_hat"),
-           br(),
-           textOutput("beta_CI"), 
-           br(), 
-           textOutput("f_test"),
-           br(),
-           textOutput("r_sq"),
-           br(),
-           textOutput("C_p")
-           
+         tabsetPanel(id = "Tabs",
+           tabPanel("Beta Coefficients", textOutput("betaC")),
+           tabPanel("Beta C-I", textOutput("betaCI")),
+           tabPanel("R Squared", textOutput("rSq")),
+           tabPanel("Mallow's Cp", textOutput("Cp")),
+           tabPanel("Plots", plotOutput("packagePlots"))
+         )
         )
     )
 )
 
 
+
 server <- function(input, output) {
     
-    generate <- eventReactive(input$generate, {
-        coeff_plot(as.matrix(mtcars[, input$independentVariables]), as.vector(mtcars[, 1]))
+    # Found this code that turns textInput into a numeric vector
+    # Reference: https://stackoverflow.com/questions/34902765/vector-input-in-shiny-r-and-then-use-it/34921859
+    # x <- as.numeric(unlist(strsplit(input$xVector,",")))
+    # y <- as.numeric(unlist(strsplit(input$yVector, ",")))
+    
+    
+    # Each tab will need to recalculate everytime the button is pressed
+    # so each output gets an eventReactive function 
+    
+    start <- eventReactive (input$start, {
+        x <- as.numeric(unlist(strsplit(input$xVector,",")))
+        y <- as.numeric(unlist(strsplit(input$yVector, ",")))
+        coeff_beta(y, x)
+        })
+    
+    start2 <- eventReactive (input$start, {
+        x <- as.numeric(unlist(strsplit(input$xVector,",")))
+        y <- as.numeric(unlist(strsplit(input$yVector, ",")))
+        coeff_ci(y, x)
     })
     
-    generate2 <- eventReactive(input$generate, {
-        coeff_beta(as.matrix(mtcars[, input$independentVariables]), as.vector(mtcars[, 1]))
+    start3 <- eventReactive (input$start, {
+        x <- as.numeric(unlist(strsplit(input$xVector,",")))
+        y <- as.numeric(unlist(strsplit(input$yVector, ",")))
+        coeff_rsquared(y, x)
     })
     
-    generate3 <- eventReactive(input$generate, {
-        coeff_ci(as.matrix(mtcars[, input$independentvariables]), as.vector(mtcars[, 1]))
+    start4 <- eventReactive (input$start, {
+        x <- as.numeric(unlist(strsplit(input$xVector,",")))
+        y <- as.numeric(unlist(strsplit(input$yVector, ",")))
+        coeff_mallows_Cp(y, x)
     })
     
-    generate4 <- eventReactive(input$generate, {
-        coeff_ftest(as.matrix(mtcars[, input$independentVariables]), as.vector(mtcars[, 1]))
+    start5 <- eventReactive (input$start, {
+        x <- as.numeric(unlist(strsplit(input$xVector,",")))
+        y <- as.numeric(unlist(strsplit(input$yVector, ",")))
+        coeff_plot(y, x)
     })
     
-    generate5 <- eventReactive(input$generate, {
-        coeff_rsquared(as.matrix(mtcars[, input$independentVariables]), as.vector(mtcars[, 1]))
+    
+    output$betaC <- renderText({
+            (start())
     })
     
-    generate6 <- eventReactive(input$generate, {
-        coeff_mallows_Cp(as.matrix(mtcars[, input$independentVariables]), as.vector(mtcars[, 1]))
+    output$betaCI <- renderText({
+        (start2())
+    })
+    
+    output$rSq <- renderText({
+        (start3())
+    })
+    
+    output$Cp <- renderText({
+        (start4())
     })
     
     output$packagePlots <- renderPlot({
-        generate()
-        }
-    )
+        (start5())
+    })
     
-    output$beta_hat<- renderText({
-        generate2()
-    }
-    )
-    
-    output$beta_ci <- renderText({
-        generate3()
-    }
-    )
-    
-    output$f_test <- renderText({
-        generate4()
-    }
-    )
-    
-    output$r_sq <- renderText({
-        generate5()
-    }
-    )
-    
-    output$c_p <- renderText({
-        generate6()
-    }
-    )
 }
-
 # Run the application 
 shinyApp(ui = ui, server = server)
